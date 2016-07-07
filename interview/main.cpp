2ce2fb2e1f3bd69d6152df104362e322f7a447d4
Amy McNeill <amymcneill@Amys-MacBook-Pro.local>
2016-07-08 07:47:15 +0900

    Initial Commit

---
+++ interview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  interview
+//
+//  Created by Amy McNeill on 7/8/16.
+//  Copyright (c) 2016 Amy McNeill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
